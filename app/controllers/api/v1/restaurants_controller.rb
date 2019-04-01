aab84aead9b3f60c0ccb225a2fead37db369678e
Praveen <maurya.praveen@gmail.com>
2019-04-01 08:46:36 +0800

    wechat mini api

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,69 @@
+class Api::V1::RestaurantsController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
+  before_action :set_restaurant, only: [ :show, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all.order(:id)
+  end
+
+  def show
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    head :no_content
+  end
+
+  def comment
+    @comment = Comment.new(comment_params)
+    @comment.restaurant = Restaurant.find(params[:restaurant_id])
+    if @comment.save
+      render :show, status: :created
+    else
+      render_error_comment
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+   def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :image, :description)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:name, :content, :image, :restaurant_id)
+  end
+
+
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+    status: :unprocessable_entity
+  end
+
+  def render_error_comment
+    render json: { errors: @comment.errors.full_messages },
+    status: :unprocessable_entity
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: [ :index, :show, :update, :create, :destroy] do
+        member do
+          post "comment"
+        end
+      end
+    end
+  end
 end
+
+
